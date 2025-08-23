f0656ad7804bfb39d1e7e94c1dd0583fa92d88f8
sahgul27 <sahilgulati.amritsar@gmail.com>
2025-08-23 13:57:07 +0530

    Fixed port

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -13,13 +13,13 @@ resource "aws_lb" "app_alb" {
 # Target Group for frontend
 resource "aws_lb_target_group" "frontend_tg" {
   name     = "${var.project_name}-frontend-tg"
-  port     = 3000
+  port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
 
   health_check {
     path                = "/"
-    port                = "3000"
+    port                = "80"
     protocol            = "HTTP"
     interval            = 30
     timeout             = 5
@@ -48,5 +48,5 @@ resource "aws_lb_listener" "http" {
 resource "aws_lb_target_group_attachment" "frontend_attach" {
   target_group_arn = aws_lb_target_group.frontend_tg.arn
   target_id        = aws_instance.frontend.id
-  port             = 3000
+  port             = 80
 }
